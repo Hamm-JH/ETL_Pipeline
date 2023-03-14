3ae498d4135b8bb3560bbc85790c3bdfaf11ae18
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-03-14 14:58:51 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import requests
+
+url = "http://ec2-3-37-12-122.ap-northeast-2.compute.amazonaws.com/api/data/log"
+response = requests.get(url)
+data = response.json()
+
+# print(data[0])
+# print(data[0]['recordId'])
+# print(data[0]['ArrivalTimeStamp'])
+print(data[0]['data']) # 복호화 대상
+print()
+print()
+print()
+
+# for i in data:
+#     print(i['recordId'])
+#     # print(i['ArrivalTimeStamp'])
+#     # print(i['data'])
+
+# from cryptography.fernet import Fernet
+# key = Fernet.generate_key()
+# print(f"대칭키:{key}")
+
+# fernet = Fernet(key)
+
+# decrypt_str = fernet.decrypt(data[0]['data'].encode())
+
+# print(decrypt_str)
+
+from cryptography.fernet import Fernet
+# key = Fernet.generate_key()
+key = b't-jdqnDewRx9kWithdsTMS21eLrri70TpkMq2A59jX8='
+print(f"대칭키:{key}")
+
+fernet = Fernet(key)
+# json_log = {"url": "/api/products/product/24", "method": "DELETE", "product_id": 24, "user_id": 21, "name": "log_file2", "inDate": "2022-12-01T01:32:21.437Z", "detail": {"message": "DELETE access Board Detail", "levelname": "INFO"}}
+# encrypt_str = fernet.encrypt(f"{json_log}".encode('ascii'))
+# print("암호화된 문자열:", encrypt_str)
+
+# decrypt_str = fernet.decrypt(encrypt_str)
+# print("복호화된 문자열: ", decrypt_str)
+
+decrypt_str = fernet.decrypt(data[0]['data'])
+print(decrypt_str)
