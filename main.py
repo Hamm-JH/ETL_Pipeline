e9628d94bc8e83713a1ca21ca9fb07fef38f0e4b
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-03-14 16:58:01 +0900

    refactor : cryptography code refactor to function

--- main.py
+++ main.py
@@ -7,29 +7,29 @@ def req_data(url):
 
     return response.json()
 
+def decrypt_data(key, data):
+    """ 대칭키와 암호화된 데이터를 받아서 복호화된 데이터를 반환하는 함수 """
+    from cryptography.fernet import Fernet
+
+    _fernet = Fernet(key)
+
+    return _fernet.decrypt(data)
+
+# requests 모듈을 사용하여 데이터를 가져온다.
 url = "http://ec2-3-37-12-122.ap-northeast-2.compute.amazonaws.com/api/data/log"
 data = req_data(url)
-
 
 # print(data[0])
 # print(data[0]['recordId'])
 # print(data[0]['ArrivalTimeStamp'])
 print(data[0]['data']) # 복호화 대상
-print()
-print()
-print()
 
 # for i in data:
 #     print(i['recordId'])
 #     # print(i['ArrivalTimeStamp'])
 #     # print(i['data'])
 
-from cryptography.fernet import Fernet
-# key = Fernet.generate_key()
+# 미리 주어진 대칭키를 이용한 복호화를 수행한다. (샘플)
 key = b't-jdqnDewRx9kWithdsTMS21eLrri70TpkMq2A59jX8='
-print(f"대칭키:{key}")
-
-fernet = Fernet(key)
-
-decrypt_str = fernet.decrypt(data[0]['data'])
+decrypt_str = decrypt_data(key, data[0]['data'])
 print(decrypt_str)
