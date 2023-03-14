2a56d2d0cf441ea9d8bbafe701a6304ce2ba3acd
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-03-15 03:25:15 +0900

    docs : create ex_timestamp_to_datetime 예시 작성

---
+++ examples/ex_timestamp_to_datetime.py
@@ -0,0 +1,15 @@
+import datetime
+
+# # String 값을 datetime object로 변경하는 방법 :: 에러 발생, 추후 점검 필요
+# date_string = '2015-07-23 18:59:09'
+# datetime.datetime.strptime('date_string', '%Y-%m-%d %H:%M:%S')
+
+# timestamp to datetime
+timestamp = 1463460958000
+datetimeobj = datetime.datetime.fromtimestamp(timestamp/1000)
+print(datetimeobj)
+
+# datetime to timestamp
+import time
+timestamp = time.mktime(datetimeobj.timetuple())
+print(timestamp)
