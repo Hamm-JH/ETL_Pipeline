b722abc9a7925a30e779e08e8705e239ac07eef5
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-03-14 16:52:41 +0900

    chore : 샘플 코드 주석 설정, main code 정리

--- examples/ex_cryptography.py
+++ examples/ex_cryptography.py
@@ -1,11 +1,19 @@
 from cryptography.fernet import Fernet
+
+# 대칭키 생성
 key = Fernet.generate_key()
 print(f"대칭키:{key}"); print()
 
+# 대칭키를 이용한 암호화 인스턴스 생성
 fernet = Fernet(key)
+
+# 암호화할 문자열
 json_log = {"url": "/api/products/product/24", "method": "DELETE", "product_id": 24, "user_id": 21, "name": "log_file2", "inDate": "2022-12-01T01:32:21.437Z", "detail": {"message": "DELETE access Board Detail", "levelname": "INFO"}}
+
+# 문자열을 바이트로 변환
 encrypt_str = fernet.encrypt(f"{json_log}".encode('ascii'))
 print("암호화된 문자열:", encrypt_str); print()
 
+# 바이트를 문자열로 변환
 decrypt_str = fernet.decrypt(encrypt_str)
 print("복호화된 문자열: ", decrypt_str)
--- main.py
+++ main.py
@@ -24,28 +24,12 @@ print()
 #     # print(i['ArrivalTimeStamp'])
 #     # print(i['data'])
 
-# from cryptography.fernet import Fernet
-# key = Fernet.generate_key()
-# print(f"대칭키:{key}")
-
-# fernet = Fernet(key)
-
-# decrypt_str = fernet.decrypt(data[0]['data'].encode())
-
-# print(decrypt_str)
-
 from cryptography.fernet import Fernet
 # key = Fernet.generate_key()
 key = b't-jdqnDewRx9kWithdsTMS21eLrri70TpkMq2A59jX8='
 print(f"대칭키:{key}")
 
 fernet = Fernet(key)
-# json_log = {"url": "/api/products/product/24", "method": "DELETE", "product_id": 24, "user_id": 21, "name": "log_file2", "inDate": "2022-12-01T01:32:21.437Z", "detail": {"message": "DELETE access Board Detail", "levelname": "INFO"}}
-# encrypt_str = fernet.encrypt(f"{json_log}".encode('ascii'))
-# print("암호화된 문자열:", encrypt_str)
-
-# decrypt_str = fernet.decrypt(encrypt_str)
-# print("복호화된 문자열: ", decrypt_str)
 
 decrypt_str = fernet.decrypt(data[0]['data'])
 print(decrypt_str)
