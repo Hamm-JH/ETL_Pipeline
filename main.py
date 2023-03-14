cb58e2738070bda7b5fabca8bc0e1d9fb664444c
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-03-15 02:33:56 +0900

    feat : b64uuid를 사용해서 uuid 문자열 길이 축소

--- main.py
+++ main.py
@@ -15,6 +15,21 @@ def decrypt_data(key, data):
 
     return _fernet.decrypt(data)
 
+def str_to_json(str):
+    """ 문자열을 json으로 변환하는 함수 """
+    import json
+
+    return json.loads(str.replace("'", "\""))
+
+def encode_b64uuid_64(b64uuid):
+    """ b64uuid를 uuid로 변환하는 함수 """
+    from b64uuid import B64UUID
+
+    start32 = B64UUID(b64uuid[:32])
+    end32 = B64UUID(b64uuid[32:])
+
+    return start32.string + end32.string
+
 def timestamp_to_datetime(timestamp):
     """ 타임스탬프를 datetime으로 변환하는 함수 """
     import datetime
@@ -31,6 +46,7 @@ data = req_data(url)
 # print(data[0]['recordId'])
 # print(data[0]['ArrivalTimeStamp'])
 print(data[0]['data']) # 복호화 대상
+print()
 
 # for i in data:
 #     print(i['recordId'])
@@ -38,19 +54,50 @@ print(data[0]['data']) # 복호화 대상
 #     # print(i['data'])
 
 # ----------------------------------------------------------------------------------------------
-
 # 미리 주어진 대칭키를 이용한 복호화를 수행한다. (샘플)
+
 key = b't-jdqnDewRx9kWithdsTMS21eLrri70TpkMq2A59jX8='
-decrypt_str = decrypt_data(key, data[0]['data'])
-print(decrypt_str)
+decrypt_str = decrypt_data(key, data[0]['data']).decode('utf-8')
+print(decrypt_str); print()
+
+# ----------------------------------------------------------------------------------------------
+# 복호화된 데이터를 json(dict)으로 변환한다.
+
+_json = str_to_json(decrypt_str)
+print(_json)
+print(type(_json))
+print(_json['user_id']) # user_id : b64uuid 사용해서 축소할 예정
+print(_json['record_id'])
+print(_json['activity'])
+print(_json['url'])
+print(_json['method']) # method : POST, GET, PUT, DELETE, 1,2,3,4로 변환할 예정
+print(_json['name'])
+print(_json['inDate']) # inDate : datetime -> timestamp로 변환할 예정
+print(_json['detail'])
+
+# ----------------------------------------------------------------------------------------------
+# uuid64 -> 문자열 길이 44만큼 축소
+
+_json['user_id'] = encode_b64uuid_64(_json['user_id'])
+# print(_json['user_id'])
 
 # ----------------------------------------------------------------------------------------------
 
-timestamp = data[0]['ArrivalTimeStamp']
+# timestamp = data[0]['ArrivalTimeStamp']
 
-# 타임스탬프를 datetime으로 변환 (decrypt_str['inDate']의 값과 동일함)
-datetime = timestamp_to_datetime(timestamp)
-print(datetime)
+# # 타임스탬프를 datetime으로 변환 (decrypt_str['inDate']의 값과 동일함)
+# datetime = timestamp_to_datetime(timestamp)
+# print(datetime)
 
+# # # 연, 월, 일, 시, 분을 출력, 데이터 저장시 사용
+# # print(datetime.year)
+# # print(datetime.month)
+# # print(datetime.day)
+# # print(datetime.hour)
+# # print(datetime.minute)
 
-# print(decrypt_str.decode('utf-8'))
+# # ----------------------------------------------------------------------------------------------
+
+# from b64uuid import B64UUID
+
+# # print(decrypt_str.decode('utf-8'))
