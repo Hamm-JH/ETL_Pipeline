4b88b8b0e1cfad8cf036d562b0837437b18b6fb4
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-03-16 02:17:27 +0900

    docs : create load_json example

---
+++ examples/ex_load_json.py
@@ -0,0 +1,17 @@
+import sys
+import os
+sys.path.append(os.path.pardir)
+
+import json
+
+# Get the absolute path of the parent directory
+parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+
+# Construct the path to the private.json file
+private_json_path = os.path.join(parent_dir, 'env/private.json')
+
+# Open the file and load the JSON data
+with open(private_json_path, 'r') as f:
+    private_data = json.load(f)
+
+print(private_data)
