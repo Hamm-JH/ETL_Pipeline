4d99def09c9922ed1d7792db6d96b93e06dded5e
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-03-20 01:54:41 +0900

    feat, test : create cryptography module

--- module_test.py
+++ module_test.py
@@ -2,6 +2,8 @@
 # # print('this is modules __init__.py')
 # import modules
 
-# print('this is modules/requests_ __init__.py')
-import modules.requests_ as req
+# # print('this is modules/requests_ __init__.py')
+# import modules.requests_ as req
 
+# print('this is modules/cryptography_ __init__.py')
+import modules.cryptography_ as crypto
---
+++ modules/cryptography_/__init__.py
@@ -0,0 +1,2 @@
+
+print('this is modules/cryptography_ __init__.py')
