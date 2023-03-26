14dadbb4a035b55af6a9f23552a3c7267f32e89b
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-03-26 17:45:16 +0900

    chore : 상대경로 설정 설명 추가

--- Project/__init__.py
+++ Project/__init__.py
@@ -2,4 +2,6 @@ import sys
 import os
 
 # ./Project 폴더 위치를 추가한다 (Project 폴더에 있는 모듈을 import 할 수 있도록)
+# Project 폴더 내부의 코드 간에 import 할 때 root에서부터 참조하지 않고 
+# 상대경로를 사용한다
 sys.path.append(os.path.join(os.path.curdir, "Project")) 
