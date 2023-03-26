5379b5c53afc02f7d3758cc82868226adae4a318
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-03-26 16:56:28 +0900

    feat : create core class

---
+++ Project/Core.py
@@ -0,0 +1,25 @@
+
+from abc import abstractmethod
+
+
+class Core:
+    """ 
+    Core 클래스
+    수집하고자 하는 데이터별로 상속받아서 사용한다.
+    프로젝트 단위로 수행할 내용의 원형을 정의한다.
+    """
+    def __init__(self, env):
+        self.env = env
+        pass
+
+    def extract_url(self, url):
+        """ url에서 데이터를 추출하는 함수 """
+        pass
+
+    def transform_data(self, data):
+        """ 데이터를 변환하는 함수. 단일 데이터를 대상으로 한다. """
+        pass
+
+    def load_data(self, data):
+        """ 데이터를 저장하는 함수 """
+        pass
---
+++ core_test.py
@@ -0,0 +1,8 @@
+
+from Project.Core import Core
+
+core = Core("aaa")
+
+core.extract_url("aaa")
+
+
