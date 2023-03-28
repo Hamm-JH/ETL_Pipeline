546384e4b19c35f2efd75c2a4d487699d874ed7c
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-03-29 00:58:12 +0900

    doc: 변환 관련 todo 업데이트

--- modules/converts_/__init__.py
+++ modules/converts_/__init__.py
@@ -1,6 +1,7 @@
 
 print('this is modules/converts_ __init__.py')
 
+# TODO : 프로젝트별로 굉장히 의존성이 큰 코드는 주요 코드에 넣지 않기
 def convert_method_to_int(method):
     """ method를 int로 변환하는 함수 """
     if method == 'POST':
