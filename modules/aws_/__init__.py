76b8234d22775291f59fea2baab9e33ae12c89a7
Hamm-JH <52693438+Hamm-JH@users.noreply.github.com>
2023-04-06 11:15:00 +0900

    feat: aws_ 모듈 신규 함수 send_data 정의(기존 함수 이전)

--- modules/aws_/__init__.py
+++ modules/aws_/__init__.py
@@ -10,3 +10,14 @@ def send_to_aws_s3_path(data, file_path, accessParams):
 
     # AWS S3에 파일을 저장한다.
     s3.Object(accessParams['aws_s3_bucket_name'], file_path).put(Body=data)
+
+
+def send_data(data, file_path, accessParams, service='s3'):
+    """ 데이터를 AWS S3에 전송하는 함수 """
+    import boto3
+
+    # AWS S3에 접근하기 위한 클라이언트를 생성한다.
+    s3 = boto3.resource(service, aws_access_key_id=accessParams['aws_access_key_id'], aws_secret_access_key=accessParams['aws_secret_access_key'])
+
+    # AWS S3에 파일을 저장한다.
+    s3.Object(accessParams['aws_s3_bucket_name'], file_path).put(Body=data)
