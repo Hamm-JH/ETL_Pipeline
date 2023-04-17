b6299f7a98872f2d7114d167c634a3f216cbb9b0
JiookHEO <wldnr7166@gmail.com>
2023-04-17 13:57:52 +0900

    23.04.17

--- Project/ETL_stream.py
+++ Project/ETL_stream.py
@@ -1,140 +1,6 @@
-from Core import Core
-
-class ETL_SG(Core):
-    """
-    SG sub_project etl pipeline
-    """
-    def __init__(self, env):
-        super().__init__(env)
-
-    def _extract_url(self, url, param = None):
-        import cp2_modules.extract_ as ext
-
-        return ext.extract(url, param)
-    
-    def _extract_data(self, date):
-        """
-        특수한 경우,
-        여러번의 _extract_url 함수 호출로 전체 데이터를 추출
-        """
-        import os
-        import math
-        import json
-        import pandas as pd
-        from dotenv import load_dotenv
-        load_dotenv()
-
-        api_id = os.getenv('garak_id')
-        api_pw = os.getenv('garak_passwd')
-        url = 'http://www.garak.co.kr/publicdata/dataOpen.do?'
-
-        bubin_list = ['11000101','11000102','11000103','11000104','11000105','11000106']
-        pummok_list = ['감귤','감자','건고추','고구마','단감','당근','딸기','마늘','무',
-                        '미나리','바나나','배','배추','버섯','사과','상추','생고추','수박',
-                        '시금치','양배추','양상추','양파','오이','참외','토마토','파',
-                        '포도','피망','호박']
-
-        dict1 = {'data': []}
-        for pummok in pummok_list:
-            dict2 = {f'{pummok}': []}
-            for bubin in bubin_list:
-                params = (
-                        ('id', api_id),
-                        ('passwd', api_pw),
-                        ('dataid', 'data12'),
-                        ('pagesize', '10'),
-                        ('pageidx', '1'),
-                        ('portal.templet', 'false'),
-                        ('s_date', date),
-                        ('s_bubin', bubin),
-                        ('s_pummok', pummok),
-                        ('s_sangi', '')
-                        )
-                dict3 = {f'{bubin}': []}
-                list_total_count = int(ETL_CP2._extract_url(self, url, param = None)['lists']['list_total_count'])
-                total_page = math.ceil(int(list_total_count) / 10)
-
-                if int(list_total_count) != 0:
-                    for page in range(1, total_page+1):
-                        params = (
-                                    ('id', api_id),
-                                    ('passwd', api_pw),
-                                    ('dataid', 'data12'),
-                                    ('pagesize', '10'),
-                                    ('pageidx', page),
-                                    ('portal.templet', 'false'),
-                                    ('s_date', date),
-                                    ('s_bubin', bubin),
-                                    ('s_pummok', pummok),
-                                    ('s_sangi', '')
-                                )
-                        html_dict = ETL_CP2._extract_url(url, params)
-                        if list_total_count % 10 > 1:
-                            for i in range(len(html_dict['lists']['list'])):
-                                dict3[f'{bubin}'].append({
-                                    'idx' : ((page -1) * 10) + (i + 1),
-                                    'PUMMOK' : html_dict['lists']['list'][i]['PUMMOK'],
-                                    'PUMJONG' : html_dict['lists']['list'][i]['PUMJONG'],
-                                    'UUN' : html_dict['lists']['list'][i]['UUN'],
-                                    'DDD' : html_dict['lists']['list'][i]['DDD'],
-                                    'PPRICE' : html_dict['lists']['list'][i]['PPRICE'],
-                                    'SSANGI' : html_dict['lists']['list'][i]['SSANGI'],
-                                    'CORP_NM' : html_dict['lists']['list'][i]['CORP_NM'],
-                                    'ADJ_DT' : html_dict['lists']['list'][i]['ADJ_DT']
-                                    })
-                        elif list_total_count % 10 == 1:
-                            if list_total_count > 1:
-                                for i in range(10):
-                                    dict3[f'{bubin}'].append({
-                                        'idx' : ((page -1) * 10) + (i + 1),
-                                        'PUMMOK' : html_dict['lists']['list'][i]['PUMMOK'],
-                                        'PUMJONG' : html_dict['lists']['list'][i]['PUMJONG'],
-                                        'UUN' : html_dict['lists']['list'][i]['UUN'],
-                                        'DDD' : html_dict['lists']['list'][i]['DDD'],
-                                        'PPRICE' : html_dict['lists']['list'][i]['PPRICE'],
-                                        'SSANGI' : html_dict['lists']['list'][i]['SSANGI'],
-                                        'CORP_NM' : html_dict['lists']['list'][i]['CORP_NM'],
-                                        'ADJ_DT' : html_dict['lists']['list'][i]['ADJ_DT']
-                                        })
-                                list_total_count -= 10
-                            elif list_total_count == 1:
-                                dict3[f'{bubin}'].append({
-                                    'idx' : int(html_dict['lists']['list_total_count']),
-                                    'PUMMOK' : html_dict['lists']['list']['PUMMOK'],
-                                    'PUMJONG' : html_dict['lists']['list']['PUMJONG'],
-                                    'UUN' : html_dict['lists']['list']['UUN'],
-                                    'DDD' : html_dict['lists']['list']['DDD'],
-                                    'PPRICE' : html_dict['lists']['list']['PPRICE'],
-                                    'SSANGI' : html_dict['lists']['list']['SSANGI'],
-                                    'CORP_NM' : html_dict['lists']['list']['CORP_NM'],
-                                    'ADJ_DT' : html_dict['lists']['list']['ADJ_DT']
-                                    })
-                    dict2[f'{pummok}'].append(dict3)
-                else:
-                    pass
-            dict1['data'].append(dict2)
-
-        flattened_data = []
-        for item_data in dict1['data']:
-            for item, bubin_list in item_data.items():
-                for bubin_data in bubin_list:
-                    for bubin, transactions in bubin_data.items():
-                        for transaction in transactions:
-                            flattened_row = transaction.copy()
-                            flattened_row['item'] = item
-                            flattened_row['bubin'] = bubin
-                            flattened_data.append(flattened_row)
-
-        return flattened_data
-    
-    def _load_data(self, data, aws_service_name, region, aws_access_id, aws_secret_key, aws_bucket_name, partitioning_func = None):
-        """
-        Spark 를 이용한 데이터 처리 및 분석 가능
-        (DataFrame 형태로 반환)
-        """
-        import cp2_modules.load_ as aws
-
-        aws.s3_load(data, aws_service_name, region, aws_access_id, aws_secret_key, aws_bucket_name, partitioning_func = None)
+from ETL_SG import ETL_SG
+# from pyspark.sql.types import *
+# from pyspark.sql import SparkSession
 
 
 AWS_SERVICE_NAME = 's3'
@@ -143,6 +9,8 @@ REGION = "ap-northeast-2"
 AWS_ACCESS_ID = 'aws_access_key_id'
 AWS_SECRET_KEY = 'aws_secret_access_key'
 AWS_BUCKET_NAME = 'aws_s3_bucket_name'
+
+etl = ETL_SG()
 
 def partitioning(flattened_data):
     if len(flattened_data) != 0:
@@ -156,8 +24,16 @@ def partitioning(flattened_data):
 
 
 def etl_stream(date):
-    flattened_data = ETL_SG._extract_data(date)      
-    ETL_SG._load_data(flattened_data, AWS_SERVICE_NAME, REGION, AWS_ACCESS_ID, AWS_SECRET_KEY, AWS_BUCKET_NAME, partitioning)
+    flattened_data = etl._extract_data(date)
+
+    # schema 설정
+    # sparkSession open
+
+    # spark 처리 코드 작성가능
+
+    etl._load_data(flattened_data, AWS_SERVICE_NAME, REGION, AWS_ACCESS_ID, AWS_SECRET_KEY, AWS_BUCKET_NAME, partitioning)
         
 etl_stream('20230407')
+
+# spark.stop()
     
